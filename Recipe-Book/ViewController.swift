e3bf6b7c89c069de2b178dabef4e94b80f48a5bf
Сергей Куклин <huvalk@MacBook-Pro-Sergej.local>
2020-10-13 23:14:46 +0300

    feat: login screen ready

--- Recipe-Book/ViewController.swift
+++ Recipe-Book/ViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var lostPswdButton: UIButton!
+    @IBOutlet weak var joinButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
