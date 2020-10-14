6a995888c90bc7612a2f545f2669d17904ccb6fd
Сергей Куклин <huvalk@MacBook-Pro-Sergej.local>
2020-10-14 09:46:28 +0300

    feat(loginViewController):keyboard shift

---
+++ Recipe-Book/LoginViewController.swift
@@ -0,0 +1,51 @@
+//
+//  LoginViewController.swift
+//  Recipe-Book
+//
+//  Created by Сергей Куклин on 14.10.2020.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var loginLabel: UILabel!
+    @IBOutlet weak var shiftConstraint: NSLayoutConstraint!
+    @IBOutlet weak var inputContainer: UIView!
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var lostPswdButton: UIButton!
+    @IBOutlet weak var joinButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initScreen()
+        
+        registerForKeyboardNotifications()
+    }
+    
+    private func initScreen() {
+        loginButton.layer.cornerRadius = 15
+    }
+    
+    private func registerForKeyboardNotifications() {
+        //Adding notifies on keyboard appearing
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWasShown), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillBeHidden), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc private func keyboardWasShown(notification: Notification) {
+        loginLabel.isHidden = true
+        guard let info = notification.userInfo else {
+            shiftConstraint.constant = -100
+            return
+        }
+        let keyboardSize = (info[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue.size
+        shiftConstraint.constant = -(keyboardSize?.height ?? 100) / 2
+    }
+    
+    @objc private func keyboardWillBeHidden(notification: Notification) {
+        loginLabel.isHidden = false
+        shiftConstraint.constant = 0
+    }
+}
